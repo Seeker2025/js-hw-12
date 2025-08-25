7ba78b334e52302d5a7fc548dd4153d2883b2338
Igor Yurovsky <85400723+Seeker2025@users.noreply.github.com>
2025-08-25 15:10:21 +0300

    go on

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,4 +1,27 @@
+import axios from 'axios';
 
-function getImagesByQuery(query, page){
+const myApiKey = '51186890-e1c8ef6e5ef4b08950db17a2f';
+export const gallery = document.querySelector('.gallery');
+
+
+const BASE_URL ='https://pixabay.com/api/';
+
+
+export function getImagesByQuery(query, page){
+  axios.get(BASE_URL, {
+  
+    params: {
+        key: myApiKey,
+        q: query,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        // page: myPage,
+        // per_page: 40,
+        }
+ }).then(response =>{
+    
+    console.log(response.data.hits);
+});
 
 }
