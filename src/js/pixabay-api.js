ca6a95b16fdeff1267cff8d295b70078452c5b26
Igor Yurovsky <85400723+Seeker2025@users.noreply.github.com>
2025-08-27 10:34:30 +0300

    go on

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,4 +1,8 @@
 import axios from 'axios';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+import { createGallery } from './render-function.js';
 
 const myApiKey = '51186890-e1c8ef6e5ef4b08950db17a2f';
 export const gallery = document.querySelector('.gallery');
@@ -22,6 +26,17 @@ export function getImagesByQuery(query, page){
  }).then(response =>{
     
     console.log(response.data.hits);
+    createGallery(response.data.hits);
+
+ const lightbox = new SimpleLightbox('li.gallery_item a', {
+    
+            captionsData: 'alt',    
+            captionDelay: 200,    
+            animationSpeed: 250,    
+            scaleImageToRatio: true,
+
+   });
+
 });
 
 }
--- src/js/render-function.js
+++ src/js/render-function.js
@@ -1,6 +1,45 @@
-function createGallery(images){
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const gallery = document.querySelector('.gallery');
+
+
+    export function createGallery(images){
+   
+          let arr = null;
+     arr = images.map(itm =>`
+     <li class="gallery_item">
+         <a href="${itm.largeImageURL}">
+            <img class="img_itm" src="${itm.webformatURL}" alt="${itm.tags}" title="">
+         </a>
+         <div class="block">
+            <div class="one_item">
+               <h2>Likes</h2>
+               <p>${itm.likes}</p>
+            </div>
+
+            <div class="one_item">
+               <h2>View</h2>
+               <p>${itm.views}</p>
+            </div>
+
+            <div class="one_item">
+               <h2>Comments</h2>
+               <p>${itm.comments}</p>
+            </div>
+
+            <div class="one_item">
+               <h2>Downloads</h2>
+               <p>${itm.downloads}</p>
+            </div>
+         </div>      
+     </li>   
+        `
+     ).join('');
+     gallery.insertAdjacentHTML('beforeend', arr);
 
 }
+
 
 function clearGallery(){
 
